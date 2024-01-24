61aafde9ea912026c27743ba87b8074a17e1a38e
Yres0 <andresjprzv@outlook.com>
2024-01-23 23:00:18 -0600

    Primera etapa

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+// let numeroSecreto = 6;
+// let numeroUsuario = prompt("Me indicas un nï¿½mero por favor:");
+
+// console.log(numeroUsuario);
+
+// if (numeroUsuario == numeroSecreto) {
+    // alert('Acertaste el nï¿½mero');
+// }
+
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número por favor:");
 
-console.log(numeroUsuario);
+// alert('Hola Mundo'); //Muestra un mensaje en pantalla
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el número');
+let numeroUsuario = prompt("Me puedes indicar un numero, por favor"); //prompt solicita algo al usuario
+//let es para asignar variables, se usa camellCase
+
+console.log(numeroUsuario); //Almacena una variable o valor en la consola F12 para abrir la consola desde el navegador
+
+if (numeroSecreto == numeroUsuario) {
+    alert("Felicidades, acertaste el numero");
+} else {
+    alert("Sigue intentandolo");
 }
+// = asignacion y == igual a, para hacer if
